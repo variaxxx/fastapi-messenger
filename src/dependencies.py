9b0f798dd9e6cab7d6d722e0268108b952694068
Anton <variaxxx@ya.ru>
2025-11-24 18:22:08 +0300

    fix: get async db session

--- src/dependencies.py
+++ src/dependencies.py
@@ -6,5 +6,5 @@ from db.database import async_session_maker
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    with async_session_maker() as session:
+    async with async_session_maker() as session:
         yield session
