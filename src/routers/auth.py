49be06a3dc15997cf47ee2a118c8e2233bb1a1c1
Anton <variaxxx@ya.ru>
2025-11-26 17:44:08 +0300

    fix: response models

--- src/routers/auth.py
+++ src/routers/auth.py
@@ -5,20 +5,19 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 import src.services.google_oauth as auth_service
 from src.dependencies import extract_bearer_token, get_async_session
-from src.response import ResponseStructure
 from src.schemas.auth import TokensResponse
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
 
-@router.get("/google", response_model=ResponseStructure[TokensResponse])
+@router.get("/google", response_model=TokensResponse)
 async def google_login(
     code: str, db: AsyncSession = Depends(get_async_session)
 ) -> TokensResponse:
     return await auth_service.google_login(db, code)
 
 
-@router.post("/refresh", response_model=ResponseStructure[TokensResponse])
+@router.post("/refresh", response_model=TokensResponse)
 async def refresh(
     token: Annotated[str, Depends(extract_bearer_token)],
     session: AsyncSession = Depends(get_async_session),
